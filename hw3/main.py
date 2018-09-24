aa3fb6cd8c3135cece50642f8150c969268244e8
Kevin <kevinzbw@gmail.com>
2018-09-23 22:28:17 -0500

    fix version

--- hw3/main.py
+++ hw3/main.py
@@ -97,7 +97,10 @@ def test(epoch):
     total = 0
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
-            inputs, targets = inputs.to(device), targets.to(device)
+            if device == "cuda":
+                inputs, targets = inputs.cuda(), targets.cuda()
+            else:
+                inputs, targets = inputs.cpu(), targets.cpu()
             outputs = net(inputs)
             loss = criterion(outputs, targets)
 
