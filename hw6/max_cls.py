0fef4d0d7185c622333616870e5d59529cf862ce
Kevin <kevinzbw@gmail.com>
2018-10-26 20:44:18 -0500

    add max fea

--- hw6/max_cls.py
+++ hw6/max_cls.py
@@ -64,7 +64,7 @@ Y = Variable(Y).to(device)
 
 lr = 0.1
 weight_decay = 0.001
-for i in xrange(200):
+for i in range(200):
     _, output = model(X)
 
     loss = -output[torch.arange(10).type(torch.int64),torch.arange(10).type(torch.int64)]
