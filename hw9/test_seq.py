12bcb99515ed5ce6994297e7967ffd5f63e9f56c
Zhenbang Wang <kevinzbw@gmail.com>
2018-11-24 16:48:12 -0600

    fix dims

--- hw9/test_seq.py
+++ hw9/test_seq.py
@@ -69,7 +69,7 @@ for i in range(len(test[0])):
     h = h5py.File(filename,'r')
     nFrames = len(h['video'])
 
-    data = np.zeros((nFrames,3,IMAGE_SIZE,IMAGE_SIZE),dtype=np.float32)
+    data = np.zeros((1, nFrames,3,IMAGE_SIZE,IMAGE_SIZE),dtype=np.float32)
 
     for j in range(nFrames):
         frame = h['video'][j]
@@ -78,10 +78,10 @@ for i in range(len(test[0])):
         frame = frame/255.0
         frame = (frame - mean)/std
         frame = frame.transpose(2,0,1)
-        data[j,:,:,:] = frame
+        data[0, j,:,:,:] = frame
     h.close()
 
-    prediction = np.zeros((nFrames,NUM_CLASSES),dtype=np.float32)
+    # prediction = np.zeros((1, nFrames,NUM_CLASSES),dtype=np.float32)
 
     # loop_i = list(range(0,nFrames,200))
     # loop_i.append(nFrames)
@@ -107,7 +107,8 @@ for i in range(len(test[0])):
         output = model(x)
     prediction = output.cpu().numpy()
     print(prediction.shape)
-    # prediction = prediction[0]
+    prediction = prediction[0]
+    print(prediction.shape)
 
     filename = filename.replace(data_directory+'UCF-101-hdf5/',prediction_directory)
     if(not os.path.isfile(filename)):
