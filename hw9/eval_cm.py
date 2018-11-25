e2be2991d108fdf2ced54cd8627623c9d8a386e5
Zhenbang Wang <kevinzbw@gmail.com>
2018-11-25 00:40:24 -0600

    add eval

--- hw9/eval_cm.py
+++ hw9/eval_cm.py
@@ -1,31 +1,5 @@
-import os
-import sys
-import time
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-#from torchvision import datasets, transforms
-from torch.autograd import Variable
-import torch.distributed as dist
-import torchvision
-
 from helper import getUCF101
-from helper import loadFrame
-
-import h5py
-import cv2
-
-from multiprocessing import Pool
 import numpy as np
-
-IMAGE_SIZE = 224
-NUM_CLASSES = 101
-batch_size = 100
-lr = 0.0001
-num_of_epochs = 10
-
 
 data_directory = '/projects/training/bauh/AR/'
 class_list, train, test = getUCF101(base_directory = data_directory)
@@ -41,5 +15,3 @@ for i in range(confusion.shape[0]):
 diag = np.diag(confusion)
 result = sorted(zip(diag, labels))
 print(result)
-for val, lab in result:
-    print(lab)
